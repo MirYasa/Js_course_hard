0f5d320ec8e97621bbe44929d6c7a463889387b7
Miron Gurevich <gurevich.miron14@gmail.com>
2020-08-09 21:49:58 +0300

    change filter method

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const num = 266219;
 let result = 1;
 const numSplit = num.toString().split('');
 
-numSplit.forEach((item) => {
-    result *= item;
+numSplit.map((item) => {
+   return result *= item;
 });
 result = result ** 3;
 console.log(result.toString().slice(0, 2));
