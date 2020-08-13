8ea93550e88dd269d9bca515a7b9ac1314cf6a99
Miron Gurevich <gurevich.miron14@gmail.com>
2020-08-13 22:45:29 +0300

    create condition for string check existence of equal numbers

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 'use strict';
-const str = prompt('Введите строку', '');
-
+const str = '                       я1zzzzz                 gggggggg                    ',
+    arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+ let re = /[A-Z, А-Я]/gi;
+    
 function hard(a) {
-    if (typeof (a) != 'string') {
-        return 'Вы ввели не строку!';
-    } else {
+    a = a.trim();
+    if (a.search(re) != -1) {
         if (a.length > 30) {
-            return a.slice(0,30) + '...';
+            return a.slice(0, 30) + '...';
         } else {
-            return a.trim();
+            return a;
         }
+    } else {
+        return 'Вы ввели число';
     }
 }
-
 console.log(hard(str));
