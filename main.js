0d6ad916b1ab5cec6ecd53eb727b08b2bdbe05fe
Miron Gurevich <gurevich.miron14@gmail.com>
2020-08-15 13:58:33 +0300

    create cycle which output numbers which start from 2 or 4, create cycle which output prime numbers

--- main.js
+++ main.js
@@ -1,33 +1,18 @@
 'use strict';
+const arr = ['12312', '45', '1354', '2593', '40123', '6432', '12356'];
 
-const enDays = ['Monday', 'Tusday', 'Wednsday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-    ruDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    lang = prompt('Введите "en" либо "ru" для того чтобы выбрать язык'),
-    arr = {
-        'ru': ruDays,
-        'en': enDays,
-        '': 'Вы не выбрали язык!',
-    },
-    namePerson = prompt('Введите имя'),
-    result = namePerson == 'Артём' ? console.log('Директор') : namePerson == 'Максим' ? console.log('Преподователь') : console.log('Студент');
-
-if (lang == 'en') {
-    console.log(enDays);
-} else if (lang == 'ru') {
-    console.log(ruDays);
-} else {
-    console.log('Вы не выбрали язык!');
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] == 4 || arr[i][0] == 2) {
+        console.log(arr[i]);
+    }
 }
 
-switch (lang) {
-    case 'ru':
-        console.log(ruDays);
-        break;
-    case 'en':
-        console.log(enDays);
-        break;
-    default:
-        console.log('Вы не выбрали язык!');
-        break;
-}
-console.log(arr[lang]);
+nextPrime:
+    for (let i = 2; i <= 100; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                continue nextPrime;
+            }
+        }
+        console.log(i + ' ' + 'Делится на 1 и на' + ' ' + i);
+    }
