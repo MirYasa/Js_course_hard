ec63138627bff627fa467e6b6a6a90bcea60547a
Miron Gurevich <gurevich.miron14@gmail.com>
2020-09-22 14:57:03 +0300

    fix (main) fix bugs

--- main.js
+++ main.js
@@ -3,14 +3,20 @@
 const enDays = ['Monday', 'Tusday', 'Wednsday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
     ruDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
     lang = prompt('Введите "en" либо "ru" для того чтобы выбрать язык'),
-    arr = lang == 'ru' ? console.log(ruDays) : lang == 'en' ? console.log(enDays) : console.log('Вы не выбрали язык'),
+    arr = {
+        'ru': ruDays,
+        'en': enDays,
+        '': 'Вы не выбрали язык!',
+    },
     namePerson = prompt('Введите имя'),
     result = namePerson == 'Артём' ? console.log('Директор') : namePerson == 'Максим' ? console.log('Преподователь') : console.log('Студент');
 
 if (lang == 'en') {
     console.log(enDays);
+    console.log(arr[lang.trim()]);
 } else if (lang == 'ru') {
     console.log(ruDays);
+    console.log(arr[lang.trim()]);
 } else {
     console.log('Вы не выбрали язык!');
 }
